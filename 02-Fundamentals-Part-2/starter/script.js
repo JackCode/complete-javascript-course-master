ec4344b39e6d469e74fd553b91c4502a90bdd5f5
Jackson Weber <jackson@jackcode.com>
2022-11-29 01:10:33 -0500

    Learning Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,12 @@
 'use strict';
 
-function fruitProcessor(apples, oranges) {
-    
+function cutFruitPieces(fruit) {
+    return fruit * 4;
 }
 
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    
+}
