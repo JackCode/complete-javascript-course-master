6fd4992b113e44c48b9fe75b2c6b0bd92766fc7b
Jackson Weber <jackson@jackcode.com>
2022-11-29 19:18:28 -0500

    Part 2 Cont'd

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,37 @@
 'use strict';
 
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+
+// }
+
+const friends = ['Patrick', 'Emily', 'Sandra'];
+console.log(friends);
+
+// const years = new Array(1992, 1907, 1885);
+// console.log(years);
+
+// console.log(friends[0]);
+// console.log(years[2]);
+
+// console.log(friends.length);
+// console.log(years[years.length-1]);
+
+// friends[2] = 'Shelby';
+// console.log(friends);
+
+// const firstName = 'Jackson';
+// const jackson = [firstName, 'Weber', 2022 - 1991, friends];
+// console.log(jackson);
+
+
+const calcAge = function (birthYear) {
+    return new Date().getFullYear() - birthYear;
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-
-    
-}
