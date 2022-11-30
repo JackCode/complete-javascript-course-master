883f2a7336f1a1b018c5d49c4385598b94d7f321
Jackson Weber <jackson@jackcode.com>
2022-11-29 19:28:46 -0500

    Part 2 Cont'd

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,8 +11,8 @@
 
 // }
 
-const friends = ['Chad', 'Jackson', 'Patrick'];
-console.log(friends);
+// const friends = ['Chad', 'Jackson', 'Patrick'];
+// console.log(friends);
 
 // const years = new Array(1992, 1907, 1885);
 // console.log(years);
@@ -31,11 +31,41 @@ console.log(friends);
 // console.log(jackson);
 
 
-const calcAge = function (birthYear) {
-    return new Date().getFullYear() - birthYear;
-}
+// const calcAge = function (birthYear) {
+//     return new Date().getFullYear() - birthYear;
+// }
 
-const years = [1990, 1991, 1996];
+// const years = [1990, 1991, 1996];
 
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
-console.log(ages);
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+// console.log(ages);
+
+const friends = ['Chad', 'Jackson', 'Patrick'];
+console.log(friends);
+
+// Add elements
+let newLength = friends.push('Willow');
+console.log(friends);
+console.log(newLength);
+
+newLength = friends.unshift('Brent');
+console.log(friends);
+console.log(newLength);
+
+// Remove elements
+const lastEntry = friends.pop();
+console.log(friends);
+console.log(lastEntry);
+
+const firstEntry = friends.shift();
+console.log(friends);
+console.log(firstEntry);
+
+// Find which index element is located
+console.log(friends.indexOf('Jackson'));
+
+// Does array include element? (strict equality)
+console.log(friends.includes('Jackson'));
+console.log(friends.includes('Willow'));
+friends.push(23);
+console.log(friends.includes('23'));
