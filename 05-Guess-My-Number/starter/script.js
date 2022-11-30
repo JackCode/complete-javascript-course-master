50b79d3d34616179b823b2826794a6d134c30026
Jackson Weber <jackson@jackcode.com>
2022-11-29 23:42:48 -0500

    add guessing logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,11 +12,30 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(`Guess: ${guess} is of type ${typeof guess}`);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '🚫 No Number!';
+  if (score > 1) {
+    if (!guess) {
+      document.querySelector('.message').textContent = '🚫 No Number!';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = '🎉 Correct Number';
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '☝🏼 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '👇🏼 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = '💥 You lost!';
   }
 });
