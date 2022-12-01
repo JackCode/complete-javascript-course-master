b282c5a743a565351689b9f78a69537fdae06bb2
Jackson Weber <jackson@jackcode.com>
2022-11-30 22:38:37 -0500

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const fun = function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+};
+
+const firstName = 'Jackson';
+calcAge(1991);
+fun.printAge();
