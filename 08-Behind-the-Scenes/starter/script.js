dd828a697b1f4b28b1c7705e2b2748453b9fff18
Jackson Weber <jackson@jackcode.com>
2022-11-30 22:54:06 -0500

    var let const hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,16 +1,56 @@
 'use strict';
 
-const fun = function calcAge(birthYear) {
-  const age = 2022 - birthYear;
+// const fun = function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
-  }
-  printAge();
-  return age;
-};
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// };
 
-const firstName = 'Jackson';
-calcAge(1991);
-fun.printAge();
+// const firstName = 'Jackson';
+// calcAge(1991);
+// fun.printAge();
+
+// HOISTING variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Jackson';
+// let job = 'manager';
+// const year = 1991;
+
+// // HOISTING functions
+// console.log(addDecl(1, 2));
+// // console.log(addExpr(1, 2));
+// console.log(addArrow(1, 2));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// let addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// Example of issue with hoisting
+
+// const numProducts = 10;
+// if (!numProducts) deleteCart();
+
+// function deleteCart() {
+//   console.log('All products deleted');
+// }
+
+var x = 1; // Creates variable on window object
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
