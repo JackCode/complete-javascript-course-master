df5bdb14b870f1ad86a39b00666476bcfcb4702a
Jackson Weber <jackson@jackcode.com>
2022-12-01 23:59:35 -0500

    commit

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -204,3 +204,14 @@ const restaurant = {
 // console.log(...str);
 
 // // spread args for functions
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) {
+  console.log(item);
+}
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+console.log([...menu.entries()]);
