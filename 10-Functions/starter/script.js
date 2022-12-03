559c01516b817698bbe1c9077c794d71b92316e3
Jackson Weber <jackson@jackcode.com>
2022-12-03 01:15:51 -0500

    default function parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = numPassengers * 199 // works for parameters before in list
+) {
+  // Old way of setting default values (ES5)
+  //   numPassengers ||= 1;
+  //   price ||= 199.0;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', undefined, 23); // How to skip parameters
