538d228637761e2b8ab64c7c870a7a86758d5657
Jackson Weber <jackson@jackcode.com>
2022-12-03 01:48:43 -0500

    returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,28 +59,44 @@
 
 /** First class and higher order functions */
 
-const oneWord = function (str) {
-  return str.replaceAll(' ', '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replaceAll(' ', '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('Javascript is the best!', upperFirstWord);
-transformer('Javascript is the best!', oneWord);
+// transformer('Javascript is the best!', upperFirstWord);
+// transformer('Javascript is the best!', oneWord);
 
-const highFive = function () {
-  console.log('👋🏼');
-};
-document.body.addEventListener('click', highFive);
+// const highFive = function () {
+//   console.log('👋🏼');
+// };
+// document.body.addEventListener('click', highFive);
 
-['Jackson', 'Marsha', 'Adam'].forEach(highFive);
+// ['Jackson', 'Marsha', 'Adam'].forEach(highFive);
+
+/** FUNCTIONS RETURNING FUNCTIONS */
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeterHey = greet('Hey');
+greeterHey('Jackson');
+greeterHey('Patrick');
+
+greet('Hello')('Jackson!');
