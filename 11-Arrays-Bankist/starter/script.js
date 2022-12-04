a0ffb8d6d36e60db53063442ece02584ee2c74fe
Jackson Weber <jackson@jackcode.com>
2022-12-04 00:45:54 -0500

    forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,6 +75,14 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+movements.forEach(function (movement, i, arr) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+
 /*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
@@ -109,15 +117,15 @@ console.log([...arr, ...arr2]);
 console.log(letters.join(' - '));
 */
 
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-// getting last element
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
-console.log(arr.at(-2));
+// // getting last element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+// console.log(arr.at(-2));
 
-console.log('Jackson'.at(0));
-console.log('Jackson'.at(-1));
+// console.log('Jackson'.at(0));
+// console.log('Jackson'.at(-1));
