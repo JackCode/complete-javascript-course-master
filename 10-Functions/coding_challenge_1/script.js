dc4f8c0d71fff0a22d27878bce8355b53e6f6174
Jackson Weber <jackson@jackcode.com>
2022-12-03 23:33:38 -0500

    cleaned challenge code

--- 10-Functions/coding_challenge_1/script.js
+++ 10-Functions/coding_challenge_1/script.js
@@ -4,53 +4,50 @@ const poll = {
   question: 'What is your favorite programming language?',
   options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
   answers: new Array(4).fill(0),
-};
 
-poll.registerNewAnswer = function () {
-  const choice = Math.trunc(
-    Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+  registerNewAnswer() {
+    const choice = Math.trunc(
+      Number(
+        prompt(
+          `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+        )
       )
-    )
-  );
+    );
 
-  !isNaN(choice) &&
-    choice >= 0 &&
-    choice <= this.options.length - 1 &&
-    this.answers[choice]++;
+    !isNaN(choice) &&
+      choice >= 0 &&
+      choice <= this.options.length - 1 &&
+      this.answers[choice]++;
 
-  this.displayResults('string');
+    this.displayResults('string');
+  },
+
+  displayResults(type) {
+    switch (type) {
+      case 'string':
+        console.log(`Poll results are ${this.answers.join(', ')}`);
+        break;
+      case 'array':
+        console.log(this.answers);
+        break;
+      default:
+        break;
+    }
+  },
 };
 
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.displayResults = function (type) {
-  switch (type) {
-    case 'string':
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-      break;
-    case 'array':
-      console.log(this.answers);
-      break;
-    default:
-      break;
-  }
-};
-
 const displayResults = poll.displayResults;
 
-const testData1 = {
-  answers: [5, 2, 3],
-};
-const testData2 = {
+const testData = {
   answers: [1, 5, 3, 9, 6, 1],
 };
 
-displayResults.call(testData1, 'string');
-displayResults.call(testData1, 'array');
+displayResults.call({ answers: [5, 2, 3] }, 'string');
+displayResults.call({ answers: [5, 2, 3] }, 'array');
 
-displayResults.call(testData2, 'string');
-displayResults.call(testData2, 'array');
+displayResults.call(testData, 'string');
+displayResults.call(testData, 'array');
