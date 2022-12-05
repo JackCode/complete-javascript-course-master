cdc30ffcaf04c1093b79de18d1918d386e0a7b4f
Jackson Weber <jackson@jackcode.com>
2022-12-04 22:46:30 -0500

    Date

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -370,3 +370,34 @@ btnSort.addEventListener('click', function (e) {
 // console.log(10000n + 10000n);
 // // console.log(465456416548618165165468416351n ** 15165153148651356415431531n);
 // console.log(2 ** 65);
+
+// // Creating a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Dec 04 2022 22:38:37'));
+// console.log(new Date('December 25, 2022'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 33));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getDay());
+// console.log(future.getHours());
+// console.log(future.getMinutes());
+// console.log(future.getSeconds());
+// console.log(future.toISOString());
+// console.log(future.getTime());
+
+// console.log(new Date(2142274980000));
+// console.log(Date.now());
+
+// future.setFullYear(2040);
+// console.log(future);
