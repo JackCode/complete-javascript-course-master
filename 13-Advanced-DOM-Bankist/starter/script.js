56f8302474099c8c3f7dbd828d284432ae6f63ff
Jackson Weber <jackson@jackcode.com>
2022-12-12 22:13:12 -0500

    DOM traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -203,3 +203,30 @@ navLinks.addEventListener('click', function (e) {
 // );
 
 // Event delegation
+const h1 = document.querySelector('h1');
+
+// going deeper (child elements)
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); // More frequently used (live)
+console.log(h1.firstElementChild);
+h1.firstElementChild.style.color = 'white';
+console.log(h1.lastElementChild);
+h1.lastElementChild.style.color = 'orangered';
+
+// going up (parents)
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)'; // freq used for event delegation
+h1.closest('h1').style.background = 'var(--gradient-primary)'; // will find itself first
+
+// siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.parentElement.children); // get all siblings including self
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+});
