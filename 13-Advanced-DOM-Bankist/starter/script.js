8ee54269c7f6890d7e3bede4bf593c337adf25ee
Jackson Weber <jackson@jackcode.com>
2022-12-12 22:58:09 -0500

    a bad way to sticky navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -122,8 +122,21 @@ const handleHover = function (e) {
 };
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
-
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+////////////////////////////////////////////////////////////////////////////////
+// Sticky Navigation
+const s1coords = section1.getBoundingClientRect();
+console.log(s1coords.top);
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > s1coords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
 
 // LECTURES
 ////////////////////////////////////////////////////////////////////////////////
