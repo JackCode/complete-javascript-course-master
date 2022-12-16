22a313de0c77493dcf5e8c99df680341247bccb2
Jackson Weber <jackson@jackcode.com>
2022-12-15 20:05:36 -0500

    click map to add markers

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -15,10 +15,35 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
+      const coords = [position.coords.latitude, position.coords.longitude];
+
+      //                  v id of html element
+      const map = L.map('map').setView(coords, 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const coords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
+
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout!')
+          .openPopup();
+      });
     },
+    // If location cannot be retrieved
     function () {
       alert('Could not get your position.');
     }
