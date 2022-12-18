267560deae47722070143c58b0abbd6ad637f8e2
Jackson Weber <jackson@jackcode.com>
2022-12-18 12:41:25 -0500

    top level await in modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -11,3 +11,25 @@ console.log('Importing Module');
 import add, { addToCart, totalPrice as price, qty } from './shoppingCart.js'; // can do, not desirable
 add('pizza', 2);
 console.log(price, qty);
+
+// Top level await in modules only as of ES2022
+// Blocks execution of entire module
+// const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await response.json();
+// console.log(data);
+// console.log('something');
+
+const getLastPost = async function () {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+lastPost.then(last => console.log(last));
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
