536a154721fae364e4da5071c7133121d17b0067
Jackson Weber <jackson@jackcode.com>
2022-12-23 23:00:48 -0500

    update

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 'use strict';
 import View from './View.js';
 
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 import icons from 'url:../../img/icons.svg';
 
 class RecipeView extends View {
@@ -108,7 +108,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ingredient.quantity ? new Fraction(ingredient.quantity) : ''
+          ingredient.quantity ? ingredient.quantity : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ingredient.unit}</span>
